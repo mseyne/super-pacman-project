47c8c8b55cd6e98c5cf8e0d34b3027d6175649fc
mseyne <michaelseyne@gmail.com>
2016-04-14 10:07:58 +0200

    fix ch1

--- v0.1/assets/Global (0)/script.ts
+++ v0.1/assets/Global (0)/script.ts
@@ -2,5 +2,5 @@
 // Pax Fabrica - Learn creative development while revisiting video game history
 // @MichaelSeyne - mseyne.github.io
 // Peer Production Licence - Free and open for peers
-// Release Version 0.1 from Tutorial Chapter 2 : Setting the game structure
+// Release Version 0.1 from Tutorial Chapter 1 : Planning the game
 
