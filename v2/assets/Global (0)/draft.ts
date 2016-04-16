b94854cefaf42dee5222fea0f2139d293fa19829
mseyne <michaelseyne@gmail.com>
2016-04-16 08:19:58 +0200

    v2

--- v2/assets/Global (0)/draft.ts
+++
@@ -1,6 +0,0 @@
-// Super Pacman - Game Development Tutorial #5
-// Pax Fabrica - Learn creative development while revisiting video game history
-// @MichaelSeyne - mseyne.github.io
-// Peer Production Licence - Free and open for peers
-// Release Version 0.2 from Tutorial Chapter 2 : Setting the game structure
-
