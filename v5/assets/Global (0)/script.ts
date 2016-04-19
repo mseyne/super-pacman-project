63f55b7ba6422ac1ee80994fefe0f1e54d170231
mseyne <michaelseyne@gmail.com>
2016-04-19 12:00:29 +0200

    add v5 from ch5

---
+++ v5/assets/Global (0)/script.ts
@@ -0,0 +1,6 @@
+// Super Pacman - Game Development Tutorial #5
+// Pax Fabrica - Learn creative development while revisiting video game history
+// @MichaelSeyne - mseyne.github.io
+// Peer Production Licence - Free and open for peers
+// Release Version 5 from Tutorial Chapter 5 : Composing the scenes
+
---
+++ v5/assets/Items (6)/Fruits (41)/Fruits (42)/script.ts
@@ -0,0 +1,10 @@
+class FruitsBehavior extends Sup.Behavior {
+  awake() {
+    
+  }
+
+  update() {
+    
+  }
+}
+Sup.registerBehavior(FruitsBehavior);
